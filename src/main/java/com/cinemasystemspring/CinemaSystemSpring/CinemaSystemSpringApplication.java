7fa8d5d23bff3c314c4f31595c50384219a61f3c
mauriLeal <mauricio12leal@gmail.com>
2025-10-04 02:05:46 -0300

    Organização de pastas e configurações básicas do Spring

---
+++ src/main/java/com/cinemasystemspring/CinemaSystemSpring/CinemaSystemSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cinemasystemspring.CinemaSystemSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaSystemSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaSystemSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cinemasystemspring/CinemaSystemSpring/CinemaSystemSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinemasystemspring.CinemaSystemSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaSystemSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
